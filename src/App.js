d620bcadbdf9536ad8d5a7c130beca2eeafec270
unknown <alaedd@gmail.com>
2021-08-06 01:11:30 +0100

    ready to deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Center, Grid, Link } from '@chakra-ui/react'
+import Card from './components/Card'
+import {
+  daniel,
+  jeanette,
+  jonathan,
+  kira,
+  patrick,
+} from './assets/images/avatars/index'
+import { data } from './helper'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Center
+      bg="gray.200"
+      py={{ base: 20, lg: 0 }}
+      px={{ base: 6, lg: 32 }}
+      h={{ base: 'auto', lg: '100vh' }}
+    >
+      <Box>
+        <Grid
+          templateRows={{ base: 'repeat(5, auto)', lg: 'repeat(2, auto)' }}
+          templateColumns={{ base: '1fr', lg: 'repeat(4, 1fr)' }}
+          gap={6}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Card
+            colSpan={{ lg: 2 }}
+            bg="violet"
+            color="lightText"
+            avatar={daniel}
+            name={data.daniel.fullName}
+            title={data.daniel.title}
+            text={data.daniel.text}
+            quotation
+          />
+          <Card
+            bg="mediumGray"
+            color="lightText"
+            avatar={jonathan}
+            name={data.jonathan.fullName}
+            title={data.jonathan.title}
+            text={data.jonathan.text}
+          />
+          <Card
+            colStart={{ lg: 1 }}
+            colEnd={{ lg: 2 }}
+            bg="white"
+            color="darkText"
+            avatar={jeanette}
+            name={data.jeanette.fullName}
+            title={data.jeanette.title}
+            text={data.jeanette.text}
+          />
+          <Card
+            colSpan={{ lg: 2 }}
+            bg="blueGray"
+            color="lightText"
+            avatar={patrick}
+            name={data.patrick.fullName}
+            title={data.patrick.title}
+            text={data.patrick.text}
+          />
+          <Card
+            rowStart={{ lg: 1 }}
+            rowEnd={{ lg: 3 }}
+            colStart={{ lg: 4 }}
+            colEnd={{ lg: 5 }}
+            bg="white"
+            color="darkText"
+            avatar={kira}
+            name={data.kira.fullName}
+            title={data.kira.title}
+            text={data.kira.text}
+          />
+        </Grid>
+        <Box fontSize="11px" textAlign="center" mt={2}>
+          Challenge by{' '}
+          <Link
+            color="hsl(228, 45%, 44%)"
+            href="https://www.frontendmentor.io?ref=challenge"
+            isExternal
+          >
+            Frontend Mentor
+          </Link>
+          . Coded by{' '}
+          <Link
+            color="hsl(228, 45%, 44%)"
+            href="https://github.com/alaedd-hammami"
+            isExternal
+          >
+            Ala Eddine
+          </Link>
+          .
+        </Box>
+      </Box>
+    </Center>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Card.js
@@ -0,0 +1,53 @@
+import {
+  Avatar,
+  Box,
+  Flex,
+  GridItem,
+  Heading,
+  Image,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+import React from 'react'
+import quotationImg from '../assets/images/quotation.svg'
+
+const Card = ({ avatar, name, title, text, quotation = false, ...rest }) => {
+  return (
+    <GridItem position="relative" rounded={8} boxShadow="lg" {...rest}>
+      {quotation && (
+        <Image
+          position="absolute"
+          top={0}
+          right={{ base: 5, sm: 20 }}
+          src={quotationImg}
+          zIndex={1}
+        />
+      )}
+      <VStack px={7} py={3} spacing={4}>
+        <Flex alignSelf="flex-start" zIndex={2}>
+          <Avatar src={avatar} />
+          <Box ml={4} fontSize="sm">
+            <Text fontWeight="bold">{name}</Text>
+            <Text opacity={0.5} fontWeight="semibold">
+              Verified Graduate
+            </Text>
+          </Box>
+        </Flex>
+        <Heading alignSelf="flex-start" size="md" zIndex={2}>
+          {title}
+        </Heading>
+        <Text
+          alignSelf="flex-start"
+          opacity={0.7}
+          fontSize="sm"
+          // fontWeight="semibold"
+          zIndex={2}
+        >
+          {text}
+        </Text>
+      </VStack>
+    </GridItem>
+  )
+}
+
+export default Card
